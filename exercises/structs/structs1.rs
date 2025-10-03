dcf6ec8dd9f255e1b8ffa6db78750836912a3d41
jxqxs <2030110294@stmail.ntu.edu.cn>
2025-10-04 01:02:50 +0800

    add clippy and generics and traits ,all 10

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -27,7 +27,6 @@ mod tests {
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
         // let green =
-        println!("test:color");
 let  green= ColorClassicStruct{
     red:0,
     green:255,
@@ -42,7 +41,7 @@ let  green= ColorClassicStruct{
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
         // let green =
-        println!("test:tuple");
+      
 let  green =ColorTupleStruct(0,255,0);
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -53,13 +52,10 @@ let  green =ColorTupleStruct(0,255,0);
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
         // let unit_like_struct =
-        println!("test:unit");
+        
         let unit_like_struct=UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
     }
-}
-fn main(){
-    println!("test");
 }
